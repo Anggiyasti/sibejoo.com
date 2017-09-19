22eb534ad3585129eec746a4dfde03b6a0fe6f82
nursofia09 <nur.sofia@mail.unpas.ac.id>
2017-09-19 08:52:06 +0700

    paket

---
+++ passinggradefront/views/daftar_passing.php
@@ -0,0 +1,155 @@
+
+        <!--/ END Template Sidebar (Left) -->
+
+        <!-- START Template Main -->
+        <section id="main" role="main">
+            <!-- START Template Container -->
+            <div class="container-fluid">
+                <!-- Page Header -->
+               
+                <!-- Page Header -->
+
+                <!-- START row -->
+
+                <!-- Page Header -->
+                
+                <!-- Page Header -->
+
+                <!-- START row -->
+                <div class="row">
+ <div class="container-fluid">
+                <!-- Page Header -->
+                <div class="page-header page-header-block">
+                    <div class="checkbox custom-checkbox pull-left">  
+                        <h3 class="panel-title"><span class="panel-icon mr5"><i class="ico-table22"></i></span> Daftar Passing grade</h3>
+                    </div>
+                    <div class="page-header-section">
+                        <!-- Toolbar -->
+                        <div class="toolbar">
+                            <ol class="breadcrumb breadcrumb-transparent nm">
+                                
+                            </ol>
+                        </div>
+                        <!--/ Toolbar -->
+                    </div>
+                </div>  
+                            <!--/ panel toolbar wrapper -->
+
+<div class="row">
+    <div class="col-lg-12">
+        <!-- /.panel-default -->
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <div class="col-md-4">       
+                    </div>
+                    <!-- form search -->                
+                    <div class="col-md-4 col-md-offset-4">
+                        <div class="form-group input-group">
+                            
+                        </div>
+                    </div>
+                    <div class="table-responsive panel-collapse pull out">
+                             <?php echo $this->session->flashdata('msg'); ?>
+                                <table class="table table-bordered" id="column-filtering" style="font-size: 13px">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Kode</th>
+                                            <th>Wilayah</th>
+                                            <th>Universitas</th>
+                                            <th>Program Studi</th>
+                                            <th>Passing Grade</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                     <?php 
+                                     $no = 1;
+                                     foreach ($data as $ps):?>
+                                        <tr>
+                                            <td><?php echo $no; ?></td>
+                                            <td><?=$ps['kode'];?></td>
+                                            <td><?=$ps['wilayah'];?></td>
+                                            <td><?=$ps['universitas'];?></td>
+                                            <td><?=$ps['prodi'];?></td>  
+                                            <td><?=$ps['passinggrade'];?></td>                   
+                 
+                                            <td class="text-center">
+                                                <!-- button toolbar -->
+                                                <div class="toolbar">
+                                                    <div class="btn-group">
+                                            
+                                    <a href="<?=base_url()?>index.php/Passinggrade/edit_pass/<?=$ps['id_passing']?>" class="btn btn-outline btn-info">Edit</a> 
+                                    <a class="btn btn-outline btn-info" href="#delete" data-toggle="modal" data-target="#delete<?php echo $ps['id_passing'];?>">Hapus</a>
+                                                               
+                                                                <!-- Modal hapus-->
+                            <div class="modal fade" id="delete<?php echo $ps['id_passing']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                <h4 class="modal-title" id="myModalLabel">Delete</h4>
+                                        </div>
+                                            <div class="modal-body">
+                                                Are you sure?
+                                            </div>
+                                        <div class="modal-footer">
+                                            <a href="<?=base_url()?>index.php/Passinggrade/delete_pass/<?=$ps['id_passing']?>" class="btn btn-default" >Yes</a>
+                                            <a href="#"class="btn btn-default" data-dismiss="modal">No</a>
+                                        </div>
+                                    </div>
+                                    <!-- /.modal-content -->
+                                </div>
+                                <!-- /.modal-dialog -->
+                            </div>
+                            <!-- /.modal -->
+                            </div>
+                                                    </div>
+
+                                                </div>
+                                                <!--/ button toolbar -->
+                                            </td>
+                                        </tr>
+                                        <?php 
+                                        $no++;
+                                        endforeach; ?>                    
+                    </tbody>
+                    <tfoot>
+                                    <tr>
+                                        <th><input type="search" class="form-control" name="search_engine" placeholder="No"></th>
+                                        <th><input type="search" class="form-control" name="search_engine" placeholder="Kode"></th>
+                                        <th><input type="search" class="form-control" name="search_engine" placeholder="Wilayah"></th>
+                                        <th><input type="search" class="form-control" name="search_engine" placeholder="Universitas"></th>
+                                        <th><input type="search" class="form-control" name="search_engine" placeholder="Program Studi"></th>
+                                        <th><input type="search" class="form-control" name="search_engine" placeholder="Passing Grade"></th>
+                                        <th><input type="search" class="form-control" name="search_engine"  disabled/></th>
+                                    </tr>
+                                </tfoot>
+                    </table>
+                </div>
+
+                </div>
+                <!-- /.table-hover -->
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+                <!--/ END row -->
+
+              
+
+            <!-- START To Top Scroller -->
+            <a href="#" class="totop animation" data-toggle="waypoints totop" data-showanim="bounceIn" data-hideanim="bounceOut" data-offset="50%"><i class="ico-angle-up"></i></a>
+            <!--/ END To Top Scroller -->
+
+        </section>
+        <!--/ END Template Main -->
+
+        <!-- START Template Sidebar (right) -->
+        
+
+    
---
+++ passinggradefront/views/v-passgrade.php
@@ -0,0 +1,92 @@
+
+<!-- Page Content -->
+      <div id="content" class="page">
+
+        <!-- Toolbar -->
+        <div id="toolbar" class="primary-color">
+          <div class="open-left" id="open-left" data-activates="slide-out-left">
+            <i class="ion-android-menu"></i>
+          </div>
+          <span class="title">Passing Grade</span>
+          <div class="open-right" id="open-right" data-activates="slide-out">
+            <i class="ion-android-person"></i>
+          </div>
+        </div>
+
+       
+        <div class="animated fadeinup delay-1">
+          <div class="page-content">
+            <!-- With Left Icon -->
+          <h4 class="p-20" style="text-align: center;">Program Studi</h4>
+
+        <?php 
+        $no1 = 1;
+        $no2 = 2;
+        $no3 = 3;
+        $no4 = 4;
+        $no5 = 5;
+        $no6 = 6;
+        $no7 = 7;
+        $no8 = 8;
+        $no9 = 9;
+        $no10 = 10;
+        $no11 = 11;
+        $no12 = 12;
+        $no13 = 13;
+        $no14 = 14;
+        $no15 = 15;
+        $no16 = 16;
+         ?>
+
+
+          <div class="page-content" style="text-align: center;">
+            <div class="sizes p-b-25">
+              <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no1;?>">21%-25%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+              <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no2;?>">26%-30%</a></span>
+            </div>
+
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no3;?>">31%-35%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no4;?>">36%-40%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no5;?>">41%-45%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no6;?>">46%-50%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no7;?>">51%-55%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no8;?>">56%-60%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no9;?>">61%-65%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no10;?>">66%-70%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no11;?>">71%-75%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no12;?>">76%-80%</a></span>
+            </div>
+            <div class="sizes p-b-25">
+            <span class="size" ><a href="<?=base_url()?>index.php/passinggrade/hasilpassing/<?=$no13;?>">81%-85%</a></span>
+            </div>
+
+          </div>
+
+          
+        </div>
+
+
+
+          
+        </div> 
